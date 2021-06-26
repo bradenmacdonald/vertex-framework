b4704c190dbc15466aa6ad3484204398125924f1
Braden MacDonald <mail@bradenm.com>
2021-06-26 10:00:52 -0700

    Add a couple simple tests

--- vertex/vertex.test.ts
+++ vertex/vertex.test.ts
@@ -1,13 +1,30 @@
-import { group, test, assertEquals, configureTestData } from "./lib/tests.ts";
+import { group, test, assertEquals, assertThrowsAsync, configureTestData } from "./lib/tests.ts";
 import { Person, testGraph } from "./test-project/index.ts";
 
 import { VNID, SlugId } from "./index.ts";
 
 group("Vertex Core", () => {
 
-    configureTestData({loadTestProjectData: true, isolateTestWrites: false});
+    group("Basic database operations", () => {
+
+        test("Can retrieve a value", async () => {
+            const result = await testGraph.read(tx => tx.run("RETURN 42 AS value"));
+            assertEquals(result.records.length, 1);
+            assertEquals(result.records[0].get("value"), 42n);
+        });
+
+        test("Can report an error message", async () => {
+            await assertThrowsAsync(
+                () => testGraph.read(tx => tx.run("RETURN tribble bibble")),
+                undefined,
+                "Invalid input 'b'",  // The 'b' at the start of "bibble" is where the parsing error occurs
+            );
+        });
+    });
 
     group("vnidForkey", () => {
+
+        configureTestData({loadTestProjectData: true, isolateTestWrites: false});
 
         test("can retrieve VNID from either key type", async () => {
             // First, check Chris Pratt's VNID
