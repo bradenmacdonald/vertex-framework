525665c39727a23b739408631e032d732cb6991d
Braden MacDonald <mail@bradenm.com>
2022-07-02 14:37:27 -0700

    Bump to Neo4j driver v4.4.6

--- vertex/deps.ts
+++ vertex/deps.ts
@@ -1,4 +1,6 @@
-import neo4j, * as Neo4j from "https://raw.githubusercontent.com/bradenmacdonald/deno-neo4j-lite-client/4.4.1-preview2/mod.ts";
+import neo4j, * as Neo4j from "https://deno.land/x/neo4j_lite_client@4.4.6/mod.ts";
+// For local development:
+// import neo4j, * as Neo4j from "../../deno-neo4j-lite-client/mod.ts";
 export {
     neo4j,
     Neo4j,
